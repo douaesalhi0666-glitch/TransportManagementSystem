552344acc2c5e45b897b87cdb932457141dbf758
MaryemDraoui <meryamdraoui58@gmail.com>
2026-05-11 11:03:58 +0100

    .

---
+++ TransportManagementSystem/Migrations/20260511100034_FixTrajectoryStopIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransportManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTrajectoryStopIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
